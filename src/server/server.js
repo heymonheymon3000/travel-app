f139dac96a6b8045c58d35faa655fe48e0036ac4
Terry Parrish <tparri2@emory.edu>
2020-06-03 11:45:27 -0400

    fetch data

--- src/server/server.js
+++ src/server/server.js
@@ -13,10 +13,10 @@ app.use(cors())
 
 dotenv.config()
 
-const DARK_SKY_URL = "https://api.darksky.net/forecast/";
-const PIXABAY_API_URL = "https://pixabay.com/api/?key="+process.env.PIXABAY_API_KEY+"&q="; 
+const DARK_SKY_URL = "https://api.darksky.net/forecast/"
+const PIXABAY_API_URL = "https://pixabay.com/api/?key="+process.env.PIXABAY_API_KEY+"&q="
 
-let projectData = {};
+let projectData = {}
 
 app.get('/', function (req, res) {
     res.sendFile(resolve('dist', 'index.html'))
@@ -25,10 +25,10 @@ app.get('/', function (req, res) {
 app.post('/api/addTrip', function (req, res) {
     fetchData(req)
     .then((data) => {
-        res.status(200).send(data);
+        res.status(200).send(data)
     })
     .catch((err) => {
-        res.status(500).send(err);
+        res.status(500).send(err)
     })
 })
 
@@ -37,32 +37,32 @@ app.listen(3000, function () {
 })
 
 const fetchData = async (req) => {
-    const departureWeather = await fetch(DARK_SKY_URL + process.env.DARK_SKY_API_KEY + "/" + req.body.departure.lat + "," + req.body.departure.lng + "," + (Date.parse(req.body.departure.date)/1000) + "?exclude=minutely,hourly,daily,flags");
-    const departureView = await fetch(PIXABAY_API_URL + req.body.departure.city + "&image_type=photo&per_page=3");
-    const arrivalWeather = await fetch(DARK_SKY_URL + process.env.DARK_SKY_API_KEY + "/" + req.body.arrival.lat + "," + req.body.arrival.lng + "," + (Date.parse(req.body.arrival.date)/1000) + "?exclude=minutely,hourly,daily,flags");
-    const arrivalView =  await fetch(PIXABAY_API_URL + req.body.arrival.city + "&image_type=photo&per_page=3");
+    const departureWeather = await fetch(DARK_SKY_URL + process.env.DARK_SKY_API_KEY + "/" + req.body.departure.lat + "," + req.body.departure.lng + "," + (Date.parse(req.body.departure.date)/1000) + "?exclude=minutely,hourly,daily,flags")
+    const departureView = await fetch(PIXABAY_API_URL + req.body.departure.city + "&image_type=photo&per_page=3")
+    const arrivalWeather = await fetch(DARK_SKY_URL + process.env.DARK_SKY_API_KEY + "/" + req.body.arrival.lat + "," + req.body.arrival.lng + "," + (Date.parse(req.body.arrival.date)/1000) + "?exclude=minutely,hourly,daily,flags")
+    const arrivalView =  await fetch(PIXABAY_API_URL + req.body.arrival.city + "&image_type=photo&per_page=3")
 
     return Promise.all([departureWeather.json(), departureView.json(), arrivalWeather.json(), arrivalView.json()])
     .then((results) => {
-        projectData['depCity-city'] = req.body.departure.city;
-        projectData['depCity-date'] = new Date(req.body.departure.date+" ").toDateString();
-        projectData['depCity-lat'] = req.body.departure.lat;
-        projectData['depCity-lng'] = req.body.departure.lng;
-        projectData['depCity-summary'] = results[0].currently.summary;
-        projectData['depCity-icon'] = results[0].currently.icon;
-        projectData['depCity-temperature'] = results[0].currently.temperature;
-        projectData['depCity-webformatURL'] = results[1].hits[0].webformatURL;
-        projectData['arrCity-city'] = req.body.arrival.city;
-        projectData['arrCity-date'] = new Date(req.body.arrival.date+" ").toDateString();
-        projectData['arrCity-lat'] = req.body.arrival.lat;
-        projectData['arrCity-lng'] = req.body.arrival.lng;
-        projectData['arrCity-summary'] = results[2].currently.summary;
-        projectData['arrCity-icon'] = results[2].currently.icon;
-        projectData['arrCity-temperature'] = results[2].currently.temperature;
-        projectData['arrCity-webformatURL'] = results[3].hits[0].webformatURL;
-        return projectData;
+        projectData['depCity-city'] = req.body.departure.city
+        projectData['depCity-date'] = new Date(req.body.departure.date+" ").toDateString()
+        projectData['depCity-lat'] = req.body.departure.lat
+        projectData['depCity-lng'] = req.body.departure.lng
+        projectData['depCity-summary'] = results[0].currently.summary
+        projectData['depCity-icon'] = results[0].currently.icon
+        projectData['depCity-temperature'] = results[0].currently.temperature
+        projectData['depCity-webformatURL'] = results[1].hits[0].webformatURL
+        projectData['arrCity-city'] = req.body.arrival.city
+        projectData['arrCity-date'] = new Date(req.body.arrival.date+" ").toDateString()
+        projectData['arrCity-lat'] = req.body.arrival.lat
+        projectData['arrCity-lng'] = req.body.arrival.lng
+        projectData['arrCity-summary'] = results[2].currently.summary
+        projectData['arrCity-icon'] = results[2].currently.icon
+        projectData['arrCity-temperature'] = results[2].currently.temperature
+        projectData['arrCity-webformatURL'] = results[3].hits[0].webformatURL
+        return projectData
     })
     .catch((err) => {
-        throw err;
+        throw err
     })
 }
